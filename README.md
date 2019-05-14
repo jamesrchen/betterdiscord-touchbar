af15593e5cece2cddd2c4cd8b94b174d73ebad54
unknownguy2002 <unknownguy2002>
2019-05-14 18:34:17 +0800

    Added Guild Name

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Touch Bar integration with discord, yay!
 
 Utilises BetterDiscord.
+
+What it does now:
+--
+- Display stuff on your touch bar when you open discord(Guilds and Guild Name)
+- That's all. A few more functions are coming so stay tuned!
--- touchbar.plugin.js
+++ touchbar.plugin.js
@@ -1,5 +1,94 @@
 //META{"name":"touchbar","website":"https://github.com/unknownguy2002/betterdiscord-touchbar","source":"https://github.com/unknownguy2002/betterdiscord-touchbar/touchbar.plugin.js"}*//
 
-class touchbar{
+const { app, BrowserWindow, TouchBar, Tray, nativeImage } = require('electron').remote;
+const { TouchBarLabel, TouchBarButton, TouchBarSpacer, TouchBarScrubber } = TouchBar;
+const version = "1.0.0"
 
+class touchbar {
+    getName() { return "touchbar"; }
+    getDescription() { return "Integration with the infamous mac touchbar(Which I have... I don't know if that's good or bad haha)."; }
+    getVersion() { return version; }
+    getAuthor() { return "unknownguy2002"; }
+
+    load() {}
+
+    start() {
+        let libLoadedEvent = () => {
+            try{ this.onLibLoaded(); }
+            catch(err) { console.error(this.getName(), "fatal error, plugin could not be started!", err); try { this.stop(); } catch(err) { console.error(this.getName() + ".stop()", err); } }
+        };
+
+		let lib = document.getElementById("NeatoBurritoLibrary");
+		if(!lib) {
+			lib = document.createElement("script");
+			lib.id = "NeatoBurritoLibrary";
+			lib.type = "text/javascript";
+			lib.src = "https://rawgit.com/Metalloriff/BetterDiscordPlugins/master/Lib/NeatoBurritoLibrary.js";
+			document.head.appendChild(lib);
+		}
+		this.forceLoadTimeout = setTimeout(libLoadedEvent, 30000);
+        if(typeof window.NeatoLib !== "undefined") libLoadedEvent();
+		else lib.addEventListener("load", libLoadedEvent);
+
+    }
+    onLibLoaded(){
+        let touchBar = new TouchBar({
+                items: [
+                    new TouchBarLabel({
+                        label: "Discord Touchbar by unknownguy2002",
+                        textColor: "#99AAB5"
+                    }),
+                    new TouchBarSpacer({
+                        size: "large"
+                    }),
+                    new TouchBarLabel({
+                        label: String(Object.keys(NeatoLib.Modules.get("getGuilds").getGuilds()).length)+" Guilds",
+                        textColor: "#7289DA"
+                    }),
+                    new TouchBarSpacer({
+                        size: "large"
+                    }),
+                    new TouchBarLabel({
+                        label: "Guild Name",
+                        textColor: "#FFFFFF"
+                    }),
+                ]
+        })
+        BrowserWindow.getAllWindows()[0].setTouchBar(touchBar)
+    }
+
+
+    onSwitch(changed){
+            let guildname = "Guild Name"
+            if(BdApi.findModuleByProps("getGuild").getGuild(BdApi.findModuleByProps("getLastSelectedGuildId").getGuildId())){
+                guildname = BdApi.findModuleByProps("getGuild").getGuild(BdApi.findModuleByProps("getLastSelectedGuildId").getGuildId()).name
+            }
+            let touchBar = new TouchBar({
+                items: [
+                    new TouchBarLabel({
+                        label: "Discord Touchbar by unknownguy2002",
+                        textColor: "#99AAB5"
+                    }),
+                    new TouchBarSpacer({
+                        size: "large"
+                    }),
+                    new TouchBarLabel({
+                        label: String(Object.keys(NeatoLib.Modules.get("getGuilds").getGuilds()).length)+" Guilds",
+                        textColor: "#7289DA"
+                    }),
+                    new TouchBarSpacer({
+                        size: "large"
+                    }),
+                    new TouchBarLabel({
+                        label: guildname,
+                        textColor: "#FFFFFF"
+                    }),
+                ]
+            })
+            BrowserWindow.getAllWindows()[0].setTouchBar(touchBar)
+    }
+
+    stop(){
+        console.log("Stopped")
+    }
 }
